f0d9ef0060d75710ed64d148a6b215c3257b53fd
rpvilla <ralph.villa@gmail.com>
2022-01-31 13:10:25 +0800

    Updated app

--- API_Files/app.py
+++ API_Files/app.py
@@ -8,40 +8,39 @@ import os
 app = Flask(__name__)
 
 # load model
-#mdl = load('XGBoostModelFinal.joblib')
+mdl = load('XGBoostModelFinal.joblib')
 
 # load type scaler
-#cat_scaler = load('TypeScalerFinal.joblib')
+cat_scaler = load('TypeScalerFinal.joblib')
 
 @app.route("/is-fraud", methods=["POST"])
 def analyze():
     data = request.get_json()
-    #if os.path.exists('dumpfile.json'):
-    #    with open('dumpfile.json','r') as fout:
-    #        r = fout.readlines()[-1]
-    #        r = json.loads(r)
-    #    prev_step = r['step']
-    #    prevAmt = r['amount']
-    #    prevType = cat_scaler.transform(np.array(r['type']).reshape(1,-1))[0][0]
-    #else:
-    #    prev_step = np.nan
-    #    prevAmt = np.nan
-    #    prevType = np.nan
-    #with open('dumpfile.json','a') as fout:
-    #    json.dump(data,fout)
-    #    fout.write('\n')
-    #deltaDest = data['oldbalanceDest'] - data['newbalanceDest']
-    #type = cat_scaler.transform(np.array(data['type']).reshape(1,-1))[0][0]
-    #deltaOrig = data['newbalanceOrig'] - data['oldbalanceOrig']
-    #features = np.array([data['amount'],deltaDest,data['step'],prev_step,
-    #                    type,data['oldbalanceOrig'],data['newbalanceOrig'],data['newbalanceDest'],
-    #                    data['oldbalanceDest'],deltaOrig,prevAmt,prevType],dtype=float)
-    #pred = mdl.predict(features.reshape(1,-1))
-    #if pred == 1:
-    #    p = True
-    #else:
-    #    p = False
-    #return jsonify({"isFraud": p})
-    return jsonify(data)
+    if os.path.exists('dumpfile.json'):
+        with open('dumpfile.json','r') as fout:
+            r = fout.readlines()[-1]
+            r = json.loads(r)
+        prev_step = r['step']
+        prevAmt = r['amount']
+        prevType = cat_scaler.transform(np.array(r['type']).reshape(1,-1))[0][0]
+    else:
+        prev_step = np.nan
+        prevAmt = np.nan
+        prevType = np.nan
+    with open('dumpfile.json','a') as fout:
+        json.dump(data,fout)
+        fout.write('\n')
+    deltaDest = data['oldbalanceDest'] - data['newbalanceDest']
+    type = cat_scaler.transform(np.array(data['type']).reshape(1,-1))[0][0]
+    deltaOrig = data['newbalanceOrig'] - data['oldbalanceOrig']
+    features = np.array([data['amount'],deltaDest,data['step'],prev_step,
+                        type,data['oldbalanceOrig'],data['newbalanceOrig'],data['newbalanceDest'],
+                        data['oldbalanceDest'],deltaOrig,prevAmt,prevType],dtype=float)
+    pred = mdl.predict(features.reshape(1,-1))
+    if pred == 1:
+        p = True
+    else:
+        p = False
+    return jsonify({"isFraud": p})
 
 
